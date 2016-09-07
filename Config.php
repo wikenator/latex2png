72e5a15577e0ab75895cb4ac52012b5ba9867726
wikenator <arnold.wikey@gmail.com>
2016-09-07 07:53:45 -0700

    documentation update

--- Config.php
+++ Config.php
@@ -1,10 +1,21 @@
 <?php
 class Config {
+	// path to Asymptote
 	const ASY_PATH = '/usr/bin/asy';
+	
+	// path to ChkTeX
 	const CHKTEX_PATH = '/usr/bin/chktex';
+	
+	// path to LaTeX
 	const LATEX_PATH = '/usr/bin/latex';
+	
+	// path to DVIPS
 	const DVIPS_PATH = '/usr/bin/dvips';
+	
+	// path to DVIPNG
 	const DVIPNG_PATH = '/usr/bin/dvipng';
+	
+	// path to ImageMagick
 	const CONVERT_PATH = '/usr/bin/convert';
 
 	const CACHE_DIR = './cache';
